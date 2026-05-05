668409f652dbae1916a086c109fc83f70b9a571e
Elliott Barran <elliott.barran@gmail.com>
2026-05-05 16:10:05 -0700

    add test route

--- src/routes/cotizacion.routes.js
+++ src/routes/cotizacion.routes.js
@@ -3,6 +3,12 @@ import { crearCotizacion } from '../controllers/cotizacion.controller.js';
 
 const router = Router();
 
+// 🔥 TEST (para navegador)
+router.get('/', (req, res) => {
+  res.send('Ruta cotizaciones OK 🚀');
+});
+
+// 🔥 CREAR PDF
 router.post('/', crearCotizacion);
 
 export default router;
